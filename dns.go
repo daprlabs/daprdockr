e5f35dad37f06f17045314ee770ca660044ce957
rbn <reuben.bond@gmail.com>
2014-01-16 14:59:51 +1100

    DNS: Support for querying whole services and groups

--- dns.go
+++ dns.go
@@ -95,46 +95,44 @@ func createContainerHandler(currentInstances chan map[string]*Instance, errorCha
 
 		instances := *instances
 		for _, question := range request.Question {
-			instance := getInstanceFromQuestion(question, instances)
-			if instance == nil {
-				continue
-			}
-			switch question.Qtype {
-			case dns.TypeSRV:
-				parts := strings.SplitN(question.Name, ".", 3)
-				if len(parts) >= 3 {
-					// Extract the service, which is the container port.
-					containerPort := parts[0]
-					// Determine the host port which the service is mapped to.
-					if hostPortStr, ok := instance.PortMappings[containerPort]; ok {
-						hostPort, err := strconv.ParseUint(hostPortStr, 10, 16)
-						if err == nil {
-							target := parts[2]
-							// Construct the response.
-							response.Answer = append(response.Answer, &dns.SRV{
-								Hdr:    dns.RR_Header{Name: question.Name, Rrtype: dns.TypeSRV, Class: dns.ClassINET, Ttl: 0},
-								Target: target,
-								Port:   uint16(hostPort),
-							})
+			for _, instance := range getInstancesFromQuestion(question, instances) {
+				switch question.Qtype {
+				case dns.TypeSRV:
+					parts := strings.SplitN(question.Name, ".", 3)
+					if len(parts) >= 3 {
+						// Extract the service, which is the container port.
+						containerPort := parts[0]
+						// Determine the host port which the service is mapped to.
+						if hostPortStr, ok := instance.PortMappings[containerPort]; ok {
+							hostPort, err := strconv.ParseUint(hostPortStr, 10, 16)
+							if err == nil {
+								target := parts[2]
+								// Construct the response.
+								response.Answer = append(response.Answer, &dns.SRV{
+									Hdr:    dns.RR_Header{Name: question.Name, Rrtype: dns.TypeSRV, Class: dns.ClassINET, Ttl: 0},
+									Target: target,
+									Port:   uint16(hostPort),
+								})
+							}
 						}
 					}
-				}
-			case dns.TypeA, dns.TypeAAAA:
-				for _, addr := range instance.Addrs {
-					ip := net.ParseIP(addr)
-					switch {
-					case ip == nil:
-						continue
-					case ip.To4() != nil:
-						response.Answer = append(response.Answer, &dns.A{
-							Hdr: dns.RR_Header{Name: question.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
-							A:   ip.To4(),
-						})
-					case ip.To16() != nil:
-						response.Answer = append(response.Answer, &dns.AAAA{
-							Hdr:  dns.RR_Header{Name: question.Name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 0},
-							AAAA: ip.To16(),
-						})
+				case dns.TypeA, dns.TypeAAAA:
+					for _, addr := range instance.Addrs {
+						ip := net.ParseIP(addr)
+						switch {
+						case ip == nil:
+							continue
+						case ip.To4() != nil:
+							response.Answer = append(response.Answer, &dns.A{
+								Hdr: dns.RR_Header{Name: question.Name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 0},
+								A:   ip.To4(),
+							})
+						case ip.To16() != nil:
+							response.Answer = append(response.Answer, &dns.AAAA{
+								Hdr:  dns.RR_Header{Name: question.Name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 0},
+								AAAA: ip.To16(),
+							})
+						}
 					}
 				}
 			}
@@ -151,8 +149,33 @@ func createContainerHandler(currentInstances chan map[string]*Instance, errorCha
 	}
 }
 
-func getInstanceFromQuestion(question dns.Question, instances map[string]*Instance) (instance *Instance) {
-	instanceName := question.Name[:len(question.Name)-ContainerDomainSuffixLen]
-	instance = instances[instanceName]
+// TODO: Use a more optimal data structure for these kinds of operations.
+func getInstancesFromQuestion(question dns.Question, allInstances map[string]*Instance) (instances []*Instance) {
+	questionSansSentinel := question.Name[:len(question.Name)-ContainerDomainSuffixLen]
+	instances = make([]*Instance, 0, 4)
+	switch strings.Count(questionSansSentinel, ".") {
+	case 3: // <instance>.<service>.<group>.container
+		if instance, exists := allInstances[questionSansSentinel]; exists {
+			instances = append(instances, instance)
+		}
+	case 2: // <service>.<group>.container
+		// See TODO above. This is obviously a poor way to find matching instances.
+		for name, instance := range allInstances {
+			nameParts := strings.SplitN(name, ".", 2)
+			if len(nameParts) == 2 && nameParts[1] == question.Name {
+				instances = append(instances, instance)
+			}
+		}
+	case 1: // <group>.container
+		// See TODO above. This is obviously a poor way to find matching instances.
+		for name, instance := range allInstances {
+			nameParts := strings.SplitN(name, ".", 3)
+			if len(nameParts) == 3 && nameParts[2] == question.Name {
+				instances = append(instances, instance)
+			}
+		}
+	default:
+	}
+
 	return
 }
